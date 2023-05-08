4271c146727b3c01c8be25fbd665d361ab4bcc7c
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2023-05-08 14:00:54 +0800

    Config env

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('⚡️ Server is running!');
 });
 
-app.listen(3000, () => {
-  console.log(`Server running at https://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
