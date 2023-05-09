e25ab59c5916611d53dac41e3e0ca0014985fb2b
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2023-05-09 11:41:11 +0800

    Setting route and controller folder

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-const cors = require('cors');
+const route = require('./routes/index.js');
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
-const port = process.env.PORT;
-
-app.get('/', (req, res) => {
-  res.send('⚡️ Server is running!');
-});
+app.use(route);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
